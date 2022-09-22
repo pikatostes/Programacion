dfa02ad4b83eccac8d91ee5cd2e0166ae5157eb3
Alejandro Rios <107919596+pikatostes@users.noreply.github.com>
2022-09-22 10:50:19 +0200

    Add files via upload

---
+++ tema2/VariablesEnteras.java
@@ -0,0 +1,14 @@
+public class VariablesEnteras {
+	
+	public static void main (String[] args) {
+    int x; //x declarada como entera
+    
+    x=5; //valor asignado
+    System.out.println("El valor actual de mi variable es " + x);
+    
+    x=7;
+    System.out.println("El valor actual de mi variable es " + x);
+		
+	}
+}
+
