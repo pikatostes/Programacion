d46e1cd4c20a539f758c805efa3d5110b38fbb4b
Alejandro Rios <107919596+pikatostes@users.noreply.github.com>
2022-09-29 12:53:36 +0200

    Add files via upload

--- tema4/Ejercicio10.java
+++ tema4/Ejercicio10.java
@@ -2,7 +2,7 @@ public class Ejercicio10 {
     public static void main (String[] args){
         int dia;
         String mes;
-        String horoscopo = "";
+        String horoscopo = "hola";
 
         System.out.println("----------------------------------------------------------------------------");
         System.out.println("---------------------------------HOROSCOPO----------------------------------");
@@ -11,40 +11,55 @@ public class Ejercicio10 {
         dia = Integer.parseInt(System.console().readLine());
         System.out.println("¿En qué mes naciste?");
         mes = System.console().readLine();
-        System.out.println(mes);
-        if ((mes.equals("Diciembre") || mes.equals("Enero")){
+        if ((mes.equals("Diciembre") || mes.equals("Enero"))){
             if (dia <=22){
                 horoscopo = "Capricornio";
             }
-        } else if ((mes.equals("Enero") || mes.equals("Febrero")){
+        } else if ((mes.equals("Enero") || mes.equals("Febrero"))){
             if (dia <=21){
                 horoscopo = "Acuario";
             }
-        } else if ((mes.equals("Febrero") || mes.equals("Marzo")){
+        } else if ((mes.equals("Febrero") || mes.equals("Marzo"))){
             if (dia <=20){
                 horoscopo = "Piscis";
             }
-        } else if ((mes.equals("Marzo") || mes.equals("Abril")){
+        } else if ((mes.equals("Marzo") || mes.equals("Abril"))){
             if (dia <=21){
                 horoscopo = "Aries";
             }
-        } else if ((mes.equals("Abril") || mes.equals("Mayo")) {
+        } else if ((mes.equals("Abril") || mes.equals("Mayo"))) {
             if (dia <= 21) {
                 horoscopo = "Tauro";
             }
-        } else if ((mes.equals("Mayo") || mes.equals("Junio")) {
+        } else if ((mes.equals("Mayo") || mes.equals("Junio"))) {
             if (dia <= 22) {
                 horoscopo = "Géminis";
             }
-        } else if ((mes.equals("Junio") || mes.equals("Julio")) {
+        } else if ((mes.equals("Junio") || mes.equals("Julio"))) {
             if (dia <= 22) {
                 horoscopo = "Cáncer";
             }
-        } else if ((mes.equals("Julio") || mes.equals("Agosto")) {
+        } else if ((mes.equals("Julio") || mes.equals("Agosto"))) {
             if (dia <= 23) {
                 horoscopo = "Leo";
             }
-
+        } else if ((mes.equals("Agosto") || mes.equals("Septiembre"))) {
+            if (dia <= 23) {
+                horoscopo = "Virgo";
+            }
+        } else if ((mes.equals("Septiembre") || mes.equals("Octubre"))) {
+            if (dia <= 23) {
+                horoscopo = "Libra";
+            }
+        } else if ((mes.equals("Octubre") || mes.equals("Noviembre"))) {
+            if (dia <= 23) {
+                horoscopo = "Escorpio";
+            }
+        } else if ((mes.equals("Noviembre") || mes.equals("Diciembre"))) {
+            if (dia <= 23) {
+                horoscopo = "Sagitario";
+            }
+        }
 
         System.out.println("----------------------------------------------------------------------------");
         System.out.println("Tu horoscopo es: " + horoscopo);
