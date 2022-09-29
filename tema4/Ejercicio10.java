3460d9be69783f2212c3e47b25254c684d410ce9
Alejandro Rios <107919596+pikatostes@users.noreply.github.com>
2022-09-29 13:24:55 +0200

    Add files via upload

--- tema4/Ejercicio10.java
+++ tema4/Ejercicio10.java
@@ -12,51 +12,51 @@ public class Ejercicio10 {
         System.out.println("¿En qué mes naciste?");
         mes = System.console().readLine();
         if ((mes.equals("Diciembre") || mes.equals("Enero"))){
-            if (dia <=22){
+            if (dia >=22 || dia <=20){
                 horoscopo = "Capricornio";
             }
         } else if ((mes.equals("Enero") || mes.equals("Febrero"))){
-            if (dia <=21){
+            if (dia >=21 || dia <=19){
                 horoscopo = "Acuario";
             }
         } else if ((mes.equals("Febrero") || mes.equals("Marzo"))){
-            if (dia <=20){
+            if (dia >=20 || dia <=20){
                 horoscopo = "Piscis";
             }
         } else if ((mes.equals("Marzo") || mes.equals("Abril"))){
-            if (dia <=21){
+            if (dia >=21 || dia <=20){
                 horoscopo = "Aries";
             }
         } else if ((mes.equals("Abril") || mes.equals("Mayo"))) {
-            if (dia <= 21) {
+            if (dia >= 21 || dia <=21) {
                 horoscopo = "Tauro";
             }
         } else if ((mes.equals("Mayo") || mes.equals("Junio"))) {
-            if (dia <= 22) {
+            if (dia >= 22 || dia <=21) {
                 horoscopo = "Géminis";
             }
         } else if ((mes.equals("Junio") || mes.equals("Julio"))) {
-            if (dia <= 22) {
+            if (dia >= 22 || dia <=22) {
                 horoscopo = "Cáncer";
             }
         } else if ((mes.equals("Julio") || mes.equals("Agosto"))) {
-            if (dia <= 23) {
+            if (dia >= 23 || dia <=22) {
                 horoscopo = "Leo";
             }
         } else if ((mes.equals("Agosto") || mes.equals("Septiembre"))) {
-            if (dia <= 23) {
+            if (dia >= 23 || dia <=22) {
                 horoscopo = "Virgo";
             }
         } else if ((mes.equals("Septiembre") || mes.equals("Octubre"))) {
-            if (dia <= 23) {
+            if (dia >= 23 || dia <=22) {
                 horoscopo = "Libra";
             }
         } else if ((mes.equals("Octubre") || mes.equals("Noviembre"))) {
-            if (dia <= 23) {
+            if (dia >= 23 || dia <=22) {
                 horoscopo = "Escorpio";
             }
         } else if ((mes.equals("Noviembre") || mes.equals("Diciembre"))) {
-            if (dia <= 23) {
+            if (dia >= 23 || dia <=21) {
                 horoscopo = "Sagitario";
             }
         }
