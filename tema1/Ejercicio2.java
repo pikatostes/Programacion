050c2257cefc1a07f413c77749053eb0efc997de
Alejandro Rios <107919596+pikatostes@users.noreply.github.com>
2022-12-01 09:15:08 +0100

    Update Ejercicio2.java

--- tema1/Ejercicio2.java
+++ tema1/Ejercicio2.java
@@ -1,8 +1,8 @@
 public class Ejercicio2 {
 	public static void main (String[] args) {
 		System.out.println("Alejandro");
-    System.out.println("Avda. Tomás García Figueras, 4");
-    System.out.println("628 31 93 19");
+    		System.out.println("Avda. Tomás García Figueras, 4");
+    		System.out.println("628 31 93 19");
 	}
 }
 
